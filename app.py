fdfec1b5bf31a0cf47c36aa406c05044f9bdcf4e
Kevin Adrian <adrian.kevin@yahoo.com>
2022-10-18 23:44:01 +0800

    voices cache

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from translator import translate
 from textToSpeech import tts
 from speechToText import stt
+import os
 
 if __name__ == '__main__':
     #english to mandarin
+    cacheDir = './voicescache/' 
+    if not os.path.exists(cacheDir):
+        os.makedirs(cacheDir)
     language_origin = "en-US"
     language_destination="zh-tw"
     text = stt(language_origin) #get text from user speech via microphone with english language
--- textToSpeech.py
+++ textToSpeech.py
@@ -2,4 +2,4 @@ from gtts import gTTS
 import os
 def tts(text, language):
     tts=gTTS(text=text, lang=language)
-    tts.save('%s.mp3' % os.path.join("./voices/result"))
+    tts.save('%s.mp3' % os.path.join("./voicescache/result"))
